77d812f84ae121bab1b3c1e96e59ad0c22e2b787
kyle <kylekwon96@gmail.com>
2022-09-08 22:45:49 +0900

    220908 - learn useState hook

--- 01_리액트개발환경설정/CRA/my-app/src/App.js
+++ 01_리액트개발환경설정/CRA/my-app/src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
-// import Header from './components/Header';
-// import Welcome from './components/Welcome';
+import Header from './components/Header';
+import Welcome from './components/Welcome';
 import Counter from './components/Counter';
 
 function App() {
-  // const info = {
-  //   firstName : 'kwon',
-  //   lastName : ' kyle',
-  //   withImg : true,
-  // }
+  const info = {
+    firstName : 'kwon',
+    lastName : ' kyle',
+    withImg : true,
+  }
   return (
     <div className="App" tabIndex='0'>
-      {/* <Header title = {"Learn React A"}/>
-      <Welcome {...info}/> // withImg만 적으면, react에서 attribute의 default값은 true이다 */}
+      {/* <Header title = {"Learn React A"}/> */}
+      <Welcome {...info}/> // withImg만 적으면, react에서 attribute의 default값은 true이다
       <Counter/>
     </div>
   );
--- 01_리액트개발환경설정/CRA/my-app/src/components/Counter.js
+++ 01_리액트개발환경설정/CRA/my-app/src/components/Counter.js
@@ -2,12 +2,43 @@ import React, { useState } from 'react'
 
 export default function Counter (){
     // [값, 값을 변경할 때 쓰는 함수] useState안의 0은 처음 count의 시작값을 의미, setCount()괄호 안에는 count를 업데이트 할 값을 적어주면 된다.
-    const [count, setCount] = useState(0);
+    const operators = ['+', '-', '*'];
+
+    const [info, setInfo] = useState({
+        count : 0,
+        show: true,
+        operator: operators[0]
+    });
+
+    // const [array, setArray] = useState(['a','b','c','d']);
+
     return (
         <div>
-            <button onClick={() => setCount(count + 1)}>+1</button>
+            <button 
+            onClick={() => {
+                let result;
+                if (info.operator === '+') result = info.count + 1
+                if (info.operator === '-') result = info.count - 1
+                if (info.operator === '*') result = info.count * 1
+                setInfo({...info, count: result})
+            }}>{info.operator}1</button>
+            <button 
+                onClick={() => {
+                    // info.show = !info.show
+                    // const newInfo = info
+                    // setInfo(newInfo)
+                    setInfo({...info, show: !info.show})
+                }
+            }>Show and Hide
+            </button>
+            <button 
+            onClick={() => {
+                const idx = Math.floor(Math.random() * operators.length)
+                setInfo({...info, operator: operators[idx]})
+            }}>Change Operator</button>
             <br/>
-            Counter: {count}
+            {info.show === true && `Counter: ${info.count}`}
+            {/* Counter: {count} */}
         </div>
     )
 }
