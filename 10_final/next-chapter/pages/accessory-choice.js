a5339c32026804fc236a6f55bdb42881e24780b9
kyle <kylekwon96@gmail.com>
2022-11-16 22:15:09 +0900

    add gitignore

--- 10_final/next-chapter/pages/accessory-choice.js
+++ 10_final/next-chapter/pages/accessory-choice.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import { useRouter }from 'next/router'
 import BottomButton from "../src/components/BottomButton";
+import Radio from "../src/components/Radio";
+import RadioGroup from "../src/components/RadioGroup";
 
 export default function accessoryChoice() {
   const router  = useRouter();
   const [accessory, setAccessory] = useState('');
-  const onChangeHandler = (e) => {
-    setAccessory(e.target.value)
-  }
+  // const onChangeHandler = (e) => {
+  //   setAccessory(e.target.value)
+  // }
 
   
   return (
@@ -17,14 +19,18 @@ export default function accessoryChoice() {
         모니터와 키보드 중 원하는 액세서리를 선택해주세요.
       </h3>
     </div>
-    <label htmlFor="Monitor">
+    {/* <label htmlFor="Monitor">
       <input type="radio" name="accessory" id="accessory" value="Monitor" onChange={onChangeHandler}/>
       Monitor
     </label>
     <label htmlFor="Keyboard">
       <input type="radio" name="accessory" id="accessory" value="Keyboard" onChange={onChangeHandler}/>
       Keyboard
-    </label>
+    </label> */}
+    <RadioGroup name="accessory" onChange={(state) => console.log(state)}>
+      <Radio id="monitor" name="accessory" value="monitor" label="monitor"/>
+      <Radio id="keyboard" name="keyboard" value="keyboard" label="keyboard"/>
+    </RadioGroup>
     <BottomButton onClick={
       () => router.push('/complete')
     }>완료 화면으로 넘어가기</BottomButton>
